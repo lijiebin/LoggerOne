e6fc914afeace790d12b97676970c2f14011bd19
jimmy <noname11@126.com>
2017-06-18 01:58:25 +0800

    Replacement log message by context

--- Formatter/CommonFormatter.php
+++ Formatter/CommonFormatter.php
@@ -8,7 +8,7 @@ class CommonFormatter implements Formatter
     public function format($message, array $context = [])
     {
         $replace = [];
-        $messageWrapped = '[' . date('Y-m-d H:i:s') . ']' . " [{$context['level']}] [{$_SERVER['SERVER_NAME']}] {$message}"; 
+        $messageWrapped = '[' . date('Y-m-d H:i:s') . ']' . " [{level}] [{$_SERVER['SERVER_NAME']}] {$message}"; 
         foreach ($context as $key => $val) {
             if ( ! is_array($val) && ( ! is_object($val) || method_exists($val, '__toString'))) {
                 $replace['{' . $key . '}'] = $val;
